dd40a9e66ce7f468d3358bcbff54f6fa21b99bc0
somuip <117620797+somuip@users.noreply.github.com>
2023-07-15 10:44:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const numberOfBooksRead = () => {
 	let count =0;
 	
 	for(let i=0;i<library.length;i++){
-		if(element[i].readingStatus === true) count++;
+		if(library[i].readingStatus === true) count++;
 	}
     return count;
 };
